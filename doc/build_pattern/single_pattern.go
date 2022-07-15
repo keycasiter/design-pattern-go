21ad062e6acd26e9969517ab29363b1ee058214b
guanjian <guanjian@bytedance.com>
2022-07-15 19:48:37 +0800

    single_pattern

--- doc/build_pattern/single_pattern.go
+++ doc/build_pattern/single_pattern.go
@@ -2,6 +2,8 @@ package build_pattern
 
 import (
 	"context"
+	"fmt"
+	"sync"
 	"time"
 )
 
@@ -11,13 +13,27 @@ type Instance struct {
 
 var hungryInstance *Instance = new(Instance)
 var lazyInstance *Instance
+var once sync.Once
 
-//懒汉式
+//懒汉式-Bug版
 func GetInstanceByLazyModeWithBug(ctx context.Context) *Instance {
 	if nil == lazyInstance {
 		//模拟耗时
 		time.Sleep(time.Second)
 		lazyInstance = new(Instance)
+	}
+	return lazyInstance
+}
+
+//懒汉式-Fix版
+func GetInstanceByLazyModeWithFix(ctx context.Context) *Instance {
+	if nil == lazyInstance {
+		fmt.Println("初始化...")
+		once.Do(func() {
+			//模拟耗时
+			time.Sleep(3 * time.Second)
+			lazyInstance = new(Instance)
+		})
 	}
 	return lazyInstance
 }
--- doc/build_pattern/single_pattern_test.go
+++ doc/build_pattern/single_pattern_test.go
@@ -4,22 +4,45 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
+	"unsafe"
 )
 
 func TestGetInstanceByLazyModeWithBug(t *testing.T) {
 	ctx := context.Background()
 	cnt := 10
-	holder := make([]*Instance, 0)
 
 	for i := 0; i < cnt; i++ {
-		instance := GetInstanceByLazyModeWithBug(ctx)
-		holder = append(holder, instance)
+		go func() {
+			instance := GetInstanceByLazyModeWithBug(ctx)
+			fmt.Printf("%v\n", unsafe.Pointer(instance))
+		}()
 	}
+	time.Sleep(5 * time.Second)
+}
+
+func TestGetInstanceByLazyModeWithFix(t *testing.T) {
+	ctx := context.Background()
+	cnt := 10
+
 	for i := 0; i < cnt; i++ {
-		fmt.Printf("%v\n", &holder[i])
+		go func() {
+			instance := GetInstanceByLazyModeWithFix(ctx)
+			fmt.Printf("%v\n", unsafe.Pointer(instance))
+		}()
 	}
+	time.Sleep(5 * time.Second)
 }
 
 func TestGetInstanceByHungryMode(t *testing.T) {
+	ctx := context.Background()
+	cnt := 10
 
+	for i := 0; i < cnt; i++ {
+		go func() {
+			instance := GetInstanceByHungryMode(ctx)
+			fmt.Printf("%v\n", unsafe.Pointer(instance))
+		}()
+	}
+	time.Sleep(5 * time.Second)
 }
